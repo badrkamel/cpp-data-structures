b94b33a935679c72b127d26ccdd4b92ba9688a00
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-01-16 20:28:37 +0200

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -57,6 +57,24 @@ class LinkedList {
             temp->next = newNode;
         }
 
+        void append(int data) {
+            if (isEmpty())
+                insertFirst(data);
+
+            Node *newNode = new Node();
+            newNode->data = data;
+            newNode->next = NULL;
+
+            Node *temp = head;
+            
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = newNode;
+        }
+
         void display() {
             Node *temp = head;
             
@@ -92,6 +110,27 @@ class LinkedList {
             }
             return found;
         }
+
+        void Delete(int item) {
+            if (isEmpty()){
+                cout << "LinkedList is empty\n";
+                return;
+            }
+            Node *ptr = head;
+            if (item == head->data) {
+                head = head->next;
+                delete ptr;
+            } else {
+                Node *prev = NULL;
+                while (ptr->data != item)
+                {
+                    prev = ptr;
+                    ptr = ptr->next;
+                }
+                prev->next = ptr->next;
+
+            }
+        }
 };
 
 int main() {
@@ -104,14 +143,18 @@ int main() {
         cout << "List contains " << lst.count() << "items.\n";
     }
 
-	lst.insertFirst(10);
-	lst.insertFirst(15);
-	lst.insertFirst(25);
-	lst.display();
-	lst.insertBefore(15, 20);
-	lst.display();
-	lst.insertBefore(155, 30);
-	lst.display();
-	cout << "The List contains " << lst.count() << endl;
+    lst.insertFirst(10);
+    lst.insertFirst(15);
+    lst.insertFirst(25);
+    lst.display();
+    lst.insertBefore(15, 20);
+    lst.display();
+    lst.append(35);
+    lst.display();
+    lst.Delete(20);
+    lst.display();   
+    
+
+    cout << "The List contains " << lst.count() << endl;
 
 }
