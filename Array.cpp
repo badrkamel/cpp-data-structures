1d0438fd87148127b29757b36bc2d72abcde186d
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-01-16 12:01:21 +0200

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class Array {
+    private :
+        int size;
+        int length;
+        int *items;
+
+    public :
+
+        Array(int arrSize) {
+            size = arrSize;
+            length = 0;
+            items = new int[arrSize];
+        }
+
+        void Fill() {
+            int no_of_items;
+            cout << "How many itmes you want to fill: ";
+            cin >> no_of_items;
+
+            if (no_of_items > size) {
+                cout << "You cannot exceed the array size";
+                return;
+            }
+
+            for (int i=0; i < no_of_items; i++) {
+                
+                cout << "Enter the item: ";
+                cin >> items[i];
+                length++;
+            }
+        }
+
+        void Display() {
+            cout << "The array content\n";
+
+            for (int i=0; i < length; i++) {
+                cout << items[i] << "\t";
+            }
+            cout << endl;
+        }
+
+        int getLength() {
+            return length;
+        }
+
+        int getSize() {
+            return size;
+        }
+
+};
+
+int main() {
+    cout << "ADT for array\n";
+
+    int arrSize;
+    cout << "Enter the array size: ";
+    cin >> arrSize;
+
+    Array myArray(arrSize);
+    myArray.Fill();
+
+    cout << "Array size = " << myArray.getSize() << " while the length = " << myArray.getLength() << endl;
+
+    myArray.Display();
+}
