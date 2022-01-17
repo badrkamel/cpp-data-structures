46e654d6f015681487d649000860581a8ebb8c28
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-01-17 18:30:45 +0200

    Create my_impl_of_stack.cpp

---
+++ my_impl_of_stack.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node *next;
+};
+
+class Stack {
+    Node *top;
+
+    public:
+    Stack () {
+        top = NULL;
+    }
+    void push(Node *node) {
+        if (top==NULL) {
+            node->next = NULL;
+        } else {
+            node->next = top;
+        }
+        top = node;
+    }
+
+    void display() {
+        Node *temp = top;
+        while (temp != NULL) {
+            cout << temp->value << "\t";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+
+int main () {
+
+    // ALLOCATE THREE NODES IN THE HEAP
+    Node *one = new Node();
+    Node *two = new Node();
+    Node *three = new Node();
+
+    // Assing value values
+    one->value = 10;
+    two->value = 20;
+    three->value = 30;
+
+    Stack stk;
+    stk.push(one);
+    stk.push(two);
+    stk.push(three);
+
+    stk.display();
+}
