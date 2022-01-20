1f69e3cd091f6a8ecc934aed1871bb8c0f52cc09
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-01-20 13:36:49 +0200

    Update my_impl_of_hashtable.cpp

--- HashTable/my_impl_of_hashtable.cpp
+++ HashTable/my_impl_of_hashtable.cpp
@@ -6,18 +6,13 @@ using namespace std;
 struct Ht_item {
     int key;
     int value;
-
-    Ht_item (int k, int v) {
-        key = k;
-        value = v;
-    }
 };
 
 class HashTaple {
     int BUCKET;
     int lenght;
     int i; // for collison
-    int *items;
+    Ht_item *items;
     const double A = (sqrt(5)-1)/2;
 
 public:
@@ -25,7 +20,7 @@ public:
         BUCKET = v;
         lenght = 0;
         i = 1;
-        items = new int [BUCKET];
+        items = new Ht_item [BUCKET];
     }
 
     int Hash(int key) {
@@ -36,38 +31,47 @@ public:
         return (floor(f*BUCKET));
     }
 
-    void insert(Ht_item *ht_item) {
-
-        if (lenght == BUCKET) {
+    void insert(int key, int value) {
+        if (lenght >= BUCKET) {
             cout << "The HashTaple is full\n";
             return;
         }
-        int key = ht_item->key;
-        int value = ht_item->value;
-
+        
         int ind = Hash(key);
-        if (items[ind] != 0) {
-            while (items[ind] != 0) {
-                // cout << "Repeated index: " << ind << endl;
-                ind = ((key + (i*i)) % BUCKET);
+        if (items[ind].key != 0) { // Collision occurred
+            while (items[ind].key != 0) {
+                ind = abs((key + (i*i)) % BUCKET);
                 i++;
             }
         }
-        items[ind] = value;
+        items[ind] = {key,value};
         lenght++;
     }
 
     void getItem(int key) {
         int ind = Hash(key);
-        cout << "Value: " << items[ind] << endl;
+        if (items[ind].key != key) {
+            int i{};
+            while (items[ind].key != key) {
+                // cout << "Repeated index: " << ind << endl;
+                ind = abs((key + (i*i)) % BUCKET);
+                i++;
+                if (i>this->i) {
+                    cout << "The key: " << key << " not in the HashTable\n";
+                    return;
+                }
+            }
+        }
+
+        cout << "Value: " << items[ind].value << endl;
     }
 
     void display() {
-        cout << "HashTable Length is: " << lenght << endl;
+        cout << "HashTable contains: " << lenght << " Items." << endl;
         for (int i=0; i<BUCKET; i++) {
-            cout << i << "-> " << items[i] << " ";
+            cout << "table[" << i << "] -> {";
+            cout << items[i].key << ", " << items[i].value << "}" << endl;
         }
-        cout << endl;
     }
 
 };
@@ -79,19 +83,23 @@ int main () {
     // Create empty hashtable with 10 buckets
     HashTaple h(10);
 
-    int keys[] = {23,27,37,13,33,43,47,57,52,55,105};
-    int values[] = {10,15,20,25,30,35,40,45,50,55,60};
+    int keys[] = {23,27,37,13,33,43,47,57,52,55};
+    int values[] = {10,15,20,25,30,35,40,45,50,55};
 
     // Hashing the keys with their values
     for (int i=0; i<11; i++) {
-        Ht_item *ht_item = new Ht_item(keys[i], values[i]);
-        h.insert(ht_item);
+        h.insert(keys[i], values[i]);
     }
 
     // Retrieve item from the hashtable by his key
     h.getItem(27);
     h.getItem(33);
     h.getItem(13);
+    h.getItem(52);
+
+    // Not in the HashTable
+    h.getItem(100);
+
     
     h.display();
 }
