fbc913682da354dd10898ed773876c2ea0b1d644
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-01-25 10:57:17 +0200

    Update binary search tree

--- Trees/BST/BST.cpp
+++ Trees/BST/BST.cpp
@@ -14,10 +14,7 @@ struct Node {
 
 struct BST
 {
-	Node *root;
-	BST() {
-		root = NULL;
-	}
+	Node *root = NULL;
 
 	Node *insert(Node *pNode, int value) {
 		if (pNode==NULL) {
@@ -37,6 +34,19 @@ struct BST
 		root = insert(root, value);
 	}
 
+	bool search(Node *pnode, int key) {
+		if (pnode==NULL) {
+			return false;
+		} else if (pnode->value==key) {
+			return true;
+		} else {
+			if (key < pnode->value)
+				return search(pnode->left, key);
+			else
+				return search(pnode->right, key);
+		}
+	}
+
 	void preOrder(Node *pNode) {
 		if (pNode==NULL) {
 			return;
@@ -45,6 +55,68 @@ struct BST
 		preOrder(pNode->left);
 		preOrder(pNode->right);
 	}
+	void preOrder() {
+		cout << "Preorder:: \n";
+		preOrder(root);
+		cout << endl;
+	}
+
+	Node *MIN_Node(Node *pNode) {
+		if (pNode==NULL) {
+			cout << "Tree Is Empty\n";
+			return NULL;
+		}
+		if (pNode->left==NULL) {
+			return pNode;
+		}
+		return MIN_Node(pNode->left);
+	}
+
+	Node *MAX_Node(Node *pNode) {
+		if (pNode==NULL) {
+			cout << "Tree Is Empty\n";
+			return NULL;
+		}
+		if (pNode->right==NULL) {
+			return pNode;
+		}
+		return MAX_Node(pNode->right);
+	}
+
+	Node *remove(Node *pNode, int key) {
+		if (pNode==NULL) {
+			return NULL;
+		}
+		if (key<pNode->value)
+			pNode->left = remove(pNode->left, key);
+		else if (key>pNode->value)
+			pNode->right = remove(pNode->right, key);
+
+		else {
+			// Leaf node to delete
+			if (pNode->left == NULL && pNode->right==NULL)
+				pNode = NULL;
+
+			// one children
+			else if (pNode->left != NULL && pNode->right == NULL) {
+				Node *temp = pNode;
+				pNode = pNode->left;
+				delete temp;
+			}
+			else if (pNode->left == NULL && pNode->right != NULL) {
+				Node *temp = pNode;
+				pNode = pNode->right;
+				delete temp;
+			}
+			else {
+				Node* max = MAX_Node(pNode->left);
+				pNode->value = max->value;
+				pNode->left = remove(pNode->left, max->value);
+			}
+		}
+		return pNode;
+	}
+
 };
 
 int main() {
@@ -52,13 +124,36 @@ int main() {
 
 	BST tree;
 
+	tree.MIN_Node(tree.root);
+
 	// Insert values into BST
 	int values[10] = {34,23,22,6,16,18,57,38,95,12};
 	for (int i{}; i<10;i++) {
 		tree.insert(values[i]);
 	}
 
-	cout << "Display the Tree Content \n";
-	tree.preOrder(tree.root);
-	cout << endl;
+	Node *root = tree.root;
+
+	tree.preOrder();
+
+	tree.search(root, 4) ? cout << "Found\n" : cout << "Not found\n";
+	tree.search(root, 6) ? cout << "Found\n" : cout << "Not found\n";
+
+	tree.remove(root, 6);
+	tree.preOrder();
+	tree.remove(root, 94);
+	tree.remove(root, 95);
+
+	tree.preOrder();
+
+	Node *min = tree.MIN_Node(root);
+	if (min != NULL) {
+		cout << "The Minimum Value Is: " << min->value << endl;
+	}
+
+	Node *max = tree.MAX_Node(root);
+	if (max != NULL) {
+		cout << "The Maximun Value Is: " << max->value << endl;
+	}
+
 }
