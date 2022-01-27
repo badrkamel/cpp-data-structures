f2d2a966c3b7acd4afe3523f7d3e84135340ebea
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-01-27 16:33:25 +0200

    Update binary search tree

--- Trees/BST/BST.cpp
+++ Trees/BST/BST.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Tree Node
 struct Node {
 	int value;
 	Node *left, *right;
@@ -12,39 +13,157 @@ struct Node {
 	}
 };
 
+struct STACK_Node {
+	Node *nodePtr;
+	STACK_Node *next;
+	STACK_Node(Node *node) {
+		nodePtr = node;
+		next = NULL;
+	}
+};
+// Stack ADT to traverse iteratively
+struct Stack {
+
+	STACK_Node *top = NULL;
+
+	void push(Node *node) {
+		STACK_Node *stack_node = new STACK_Node(node);
+		stack_node->next = top;
+		top = stack_node;
+	}
+
+	int pop() {
+		if (top) {    
+			STACK_Node *temp = top;
+			int value = temp->nodePtr->value;
+
+			top = top->next;
+
+			delete temp;
+			cout << value << " ";
+			return value;
+		}
+		return -1;
+	}
+
+	int peek() {
+		if (top)
+			return top->nodePtr->value;
+		return -1;
+	}
+
+	void display() {
+		STACK_Node *temp = top;
+		cout << "Stack nodes :: [ ";
+		while (temp) {
+			cout << temp->nodePtr->value << " ";
+			temp = temp->next;
+		}
+		cout << "]\n";
+	}
+};
+
+
 struct BST
 {
-	Node *root = NULL;
+	int size;
+	int depth;
+	Node *root;
 
-	Node *insert(Node *pNode, int value) {
+	BST () {
+		size = 0;
+		depth = 0;
+		root = NULL;
+	}
+
+	Node *insertRec(Node *pNode, int value, int &d) {
 		if (pNode==NULL) {
 			Node *node = new Node(value);
 			pNode = node;
 		} else {
 			if (value < pNode->value) {
-				pNode->left = insert(pNode->left, value);
+				pNode->left = insertRec(pNode->left, value, d);
 			} else {
-				pNode->right = insert(pNode->right, value);
+				pNode->right = insertRec(pNode->right, value, d);
 			}
 		}
+		d++;
 		return pNode;
 	}
 
-	void insert(int value) {
-		root = insert(root, value);
+	void insertRec(int value) {
+		int d = 0;
+		root = insertRec(root, value, d);
+
+		if (depth<d)
+			depth=d;
+
+		size++;
 	}
 
-	bool search(Node *pnode, int key) {
+	void insertIter(int value) {
+		Node *node = new Node(value);
+		int depth = 1;
+		if (!root) {
+			root = node;
+		} else {
+			Node *temp = root;
+			while (temp)
+			{
+				depth++;
+				if (value<temp->value)
+					if (temp->left)
+						temp = temp->left;
+					else {
+						temp->left = node;
+						break;
+					}
+				else
+					if (temp->right)
+						temp = temp->right;
+					else {
+						temp->right = node;
+						break;
+					}
+			}
+		}
+
+		if (depth>this->depth)
+			this->depth = depth;
+
+		size++;
+	}
+
+	bool searchRec(Node *pnode, int key) {
 		if (pnode==NULL) {
+			printf(" - (%d) Not found\n", key);
 			return false;
 		} else if (pnode->value==key) {
+			printf(" - (%d) found\n", key);
 			return true;
 		} else {
 			if (key < pnode->value)
-				return search(pnode->left, key);
+				return searchRec(pnode->left, key);
 			else
-				return search(pnode->right, key);
+				return searchRec(pnode->right, key);
 		}
+	}
+
+	bool searchIter(int key) {
+
+		Node *temp = root;
+		while(temp) {
+			if (key==temp->value){
+				printf(" - (%d) found\n", key);
+				return true;
+			}
+			else if (key<temp->value)
+				temp = temp->left;
+			else 
+				temp = temp->right;
+		}
+		printf(" - (%d) Not found\n", key);
+		return false;
 	}
 
 	void preOrder(Node *pNode) {
@@ -56,14 +175,55 @@ struct BST
 		preOrder(pNode->right);
 	}
 	void preOrder() {
-		cout << "Preorder:: \n";
+		cout << " - PRE_Order using Recursion :: [ ";
 		preOrder(root);
-		cout << endl;
+		cout << "]\n";
+	}
+
+	void asc_inOrderIter() {
+
+		Node *temp = root;
+		if (temp) {
+			Stack s;
+			cout << " - IN_Order using Stack\n - Ascending  :: [ ";
+			do
+			{
+				while (temp)
+				{
+					s.push(temp);
+					temp = temp->left;
+				}
+				temp = s.top->nodePtr->right;
+				s.pop();
+			} while (s.top||temp);
+			cout << "]\n";
+		}
+	}
+
+
+	void desc_inOrderIter() {
+
+		Node *temp = root;
+		if (temp) {
+			Stack s;
+			cout << " - Descending :: [ ";
+			do
+			{
+				while (temp)
+				{
+					s.push(temp);
+					temp = temp->right;
+				}
+				temp = s.top->nodePtr->left;
+				s.pop();
+			} while (s.top||temp);
+			cout << "]\n";
+		}
 	}
 
 	Node *MIN_Node(Node *pNode) {
 		if (pNode==NULL) {
-			cout << "Tree Is Empty\n";
+			cout << " - Tree Is Empty\n";
 			return NULL;
 		}
 		if (pNode->left==NULL) {
@@ -74,13 +234,30 @@ struct BST
 
 	Node *MAX_Node(Node *pNode) {
 		if (pNode==NULL) {
-			cout << "Tree Is Empty\n";
+			cout << " - Tree Is Empty\n";
 			return NULL;
 		}
 		if (pNode->right==NULL) {
 			return pNode;
 		}
 		return MAX_Node(pNode->right);
+	}
+
+	int TreeDepthRec(Node *root) {
+		if (!root)
+			return 0;
+		int l = TreeDepthRec(root->left);
+		int r = TreeDepthRec(root->right);
+		
+		return (l>r) ? 1+l : 1+r;
+	}
+
+	int get_depth() {
+		return depth;
+	}
+
+	int get_size() {
+		return size;
 	}
 
 	Node *remove(Node *pNode, int key) {
@@ -93,20 +270,25 @@ struct BST
 			pNode->right = remove(pNode->right, key);
 
 		else {
+
 			// Leaf node to delete
-			if (pNode->left == NULL && pNode->right==NULL)
+			if (pNode->left == NULL && pNode->right==NULL) {
 				pNode = NULL;
+				size--;
+			}
 
 			// one children
 			else if (pNode->left != NULL && pNode->right == NULL) {
 				Node *temp = pNode;
 				pNode = pNode->left;
 				delete temp;
+				size--;
 			}
 			else if (pNode->left == NULL && pNode->right != NULL) {
 				Node *temp = pNode;
 				pNode = pNode->right;
 				delete temp;
+				size--;
 			}
 			else {
 				Node* max = MAX_Node(pNode->left);
@@ -120,40 +302,70 @@ struct BST
 };
 
 int main() {
-	cout << "BST\n\n";
+	
+	printf(
+		"\n :: Binary Search Tree ADT\n\
+		Functions :\n\
+		Insert  (Recursively & Iteratively)\n\
+		preOrder (Recursively)\n\
+		inOrderIter (Iteratively) \n\
+		MIN_Node\n\
+		MAX_Node\n\
+		TreeDepthRec\n\
+		get_depth\n\
+		get_size\n\
+		Remove\n\n"
+	);
 
 	BST tree;
+	cout << " - BST Created\n";
 
-	tree.MIN_Node(tree.root);
-
-	// Insert values into BST
-	int values[10] = {34,23,22,6,16,18,57,38,95,12};
-	for (int i{}; i<10;i++) {
-		tree.insert(values[i]);
+	/*
+		Insert values into BST (
+			Iteratively : insertIter func
+			recursively : insertRec func
+			)
+	*/
+	for (int i: {34,23,22,6,16,18,57,38,95,12}) {
+		tree.insertIter(i);
 	}
+	cout << " - 10 items inserted\n";
 
 	Node *root = tree.root;
 
 	tree.preOrder();
-
-	tree.search(root, 4) ? cout << "Found\n" : cout << "Not found\n";
-	tree.search(root, 6) ? cout << "Found\n" : cout << "Not found\n";
-
-	tree.remove(root, 6);
-	tree.preOrder();
-	tree.remove(root, 94);
-	tree.remove(root, 95);
-
-	tree.preOrder();
+	tree.asc_inOrderIter();
+	tree.desc_inOrderIter();
 
 	Node *min = tree.MIN_Node(root);
 	if (min != NULL) {
-		cout << "The Minimum Value Is: " << min->value << endl;
+		cout << " - The Minimum Value Is: " << min->value << endl;
 	}
 
 	Node *max = tree.MAX_Node(root);
 	if (max != NULL) {
-		cout << "The Maximun Value Is: " << max->value << endl;
+		cout << " - The Maximun Value Is: " << max->value << endl;
 	}
+
+	tree.searchRec(root, 4);
+	tree.searchIter(6);
+
+	// Remove some nodes
+	for (int i: {6,95}) {
+		tree.remove(root, i);
+		printf(" - (%d) removed\n", i);
+	}
+	
+	min = tree.MIN_Node(root);
+	if (min != NULL) {
+		cout << " - The Minimum Value Is: " << min->value << endl;
+	}
+
+	max = tree.MAX_Node(root);
+	if (max != NULL) {
+		cout << " - The Maximun Value Is: " << max->value << endl;
+	}
+
+	printf(" - Depth %d, Size %d\n", tree.TreeDepthRec(root), tree.get_size());
 
 }
