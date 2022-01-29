f75540aa63718c6f44c1bb541ddf73ff264dac90
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-01-30 00:42:56 +0200

    Add reverse to LinkedList

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -13,124 +13,138 @@ class Node {
 };
 
 class LinkedList {
-    Node *head;
-    public:
-        LinkedList() {
-            head = NULL;
-        }
 
-        bool isEmpty() {
-            return (head==NULL);
-        }
+    Node *head = NULL;
 
-        void insertFirst(int data) {
-            Node *newNode = new Node();
-            newNode->data = data;
+public:
+    bool isEmpty() {
+        return (head==NULL);
+    }
 
-            if (isEmpty()) {
-                newNode->next = NULL;
-            } else {
-                newNode->next = head;
-            }
+    void insertFirst(int data) {
+        Node *newNode = new Node();
+        newNode->data = data;
 
-            head = newNode;
-        }
-
-        void insertBefore(int item, int data) {
-            if (isEmpty())
-                insertFirst(data);
-            Node *newNode = new Node();
-            newNode->data = data;
-
-            Node *temp = head;
-            
-            while (temp != NULL && temp->next->data != item)
-            {
-                temp = temp->next;
-                if (temp->next == NULL){
-                    cout << "Not found!\n";
-                    return;
-                }
-            }
-
-            newNode->next = temp->next;
-            temp->next = newNode;
-        }
-
-        void append(int data) {
-            if (isEmpty())
-                insertFirst(data);
-
-            Node *newNode = new Node();
-            newNode->data = data;
+        if (isEmpty()) {
             newNode->next = NULL;
-
-            Node *temp = head;
-            
-            while (temp->next != NULL)
-            {
-                temp = temp->next;
-            }
-
-            temp->next = newNode;
+        } else {
+            newNode->next = head;
         }
 
-        void display() {
-            Node *temp = head;
-            
-            while (temp != NULL)
-            {
-                cout << temp->data << "\t";
-                temp = temp->next;
-            }
-            cout << endl;
-        }
+        head = newNode;
+    }
 
-        int count() {
+    void insertBefore(int item, int data) {
+        if (isEmpty())
+            insertFirst(data);
+        Node *newNode = new Node();
+        newNode->data = data;
 
-            int counter = 0;
-            Node *temp = head;
-            while (temp != NULL)
-            {
-                counter++;
-                temp = temp->next;
-            }
-            return counter;
-        }
-
-        bool isFound(int key) {
-
-            bool found = false;
-            Node *temp = head;
-            while (temp != NULL)
-            {
-                if (key == temp->data)
-                    found = true;
-                temp = temp->next;
-            }
-            return found;
-        }
-
-        void Delete(int item) {
-            if (isEmpty()){
-                cout << "LinkedList is empty\n";
+        Node *temp = head;
+        
+        while (temp != NULL && temp->next->data != item)
+        {
+            temp = temp->next;
+            if (temp->next == NULL){
+                cout << "Not found!\n";
                 return;
             }
-            Node *ptr = head;
-            if (item == head->data) {
-                head = head->next;
-                delete ptr;
-            } else {
-                Node *prev = NULL;
-                while (ptr->data != item)
-                {
-                    prev = ptr;
-                    ptr = ptr->next;
-                }
-                prev->next = ptr->next;
-
-            }
         }
+
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+
+    void append(int data) {
+        if (isEmpty())
+            insertFirst(data);
+
+        Node *newNode = new Node();
+        newNode->data = data;
+        newNode->next = NULL;
+
+        Node *temp = head;
+        
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+    void reverse() {
+        if (!head)
+            return;
+        Node *current = head;
+        Node *next = NULL;
+        Node *prev = NULL;
+
+        while (current) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    void display() {
+        Node *temp = head;
+        
+        while (temp != NULL)
+        {
+            cout << temp->data << "\t";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    int count() {
+
+        int counter = 0;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            counter++;
+            temp = temp->next;
+        }
+        return counter;
+    }
+
+    bool isFound(int key) {
+
+        bool found = false;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            if (key == temp->data)
+                found = true;
+            temp = temp->next;
+        }
+        return found;
+    }
+
+    void Delete(int item) {
+        if (isEmpty()){
+            cout << "LinkedList is empty\n";
+            return;
+        }
+        Node *ptr = head;
+        if (item == head->data) {
+            head = head->next;
+            delete ptr;
+        } else {
+            Node *prev = NULL;
+            while (ptr->data != item)
+            {
+                prev = ptr;
+                ptr = ptr->next;
+            }
+            prev->next = ptr->next;
+
+        }
+    }
 };
 
 int main() {
@@ -149,12 +163,14 @@ int main() {
     lst.display();
     lst.insertBefore(15, 20);
     lst.display();
-    lst.append(35);
+    lst.append(5);
     lst.display();
     lst.Delete(20);
-    lst.display();   
-    
+    cout << "The List contains: " << lst.count() << endl;
+    lst.display();
 
-    cout << "The List contains " << lst.count() << endl;
+    cout << "LinkedList After reverse: \n";
+    lst.reverse();
+    lst.display();
 
 }
