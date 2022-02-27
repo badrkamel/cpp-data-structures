4dd9bd113edbfb4ba0bb7b5ae855d2de6689dc0d
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-02-27 20:51:47 +0200

    Update circular_doubly_linkedlist.cpp

--- LinkedList/circular_doubly_linkedlist.cpp
+++ LinkedList/circular_doubly_linkedlist.cpp
@@ -6,14 +6,15 @@ struct Node {
 	Node *next = nullptr;
 	Node *prev = nullptr;
 
+	Node () : val(0) {}
 	Node (int x) : val(x) {}
 };
 
 class CircularDoublyLinkedList {
 
-	public:
 	Node *head = nullptr;
 	Node *rear = nullptr;
+	public:
 
 		// Add node
 	void insert_first(int);      // O(1)
@@ -30,6 +31,9 @@ class CircularDoublyLinkedList {
 };
 
 int main() {
+
+	std::cout << "Circular Doubly LinkedList\n";
+
 	CircularDoublyLinkedList list;
 
 	list.insert_first(20);
@@ -60,7 +64,7 @@ void CircularDoublyLinkedList::insert_first(int val) {
 	Node *node = new Node(val);
 
 	if (!head) {
-		node->prev = node->next = head = rear = node;
+		head = rear = node->next = node->prev = node;
 	} else {
 		node->next = head;
 		node->prev = rear;
@@ -94,8 +98,11 @@ void CircularDoublyLinkedList::insert_before(int item, int val) {
 
 		if (node_before->next->val == item) {
 			Node *node = new Node(val);
+
 			node->next = node_before->next;
+			node_before->next->prev = node;
 			node_before->next = node;
+			node->prev = node_before;
 		}
 	}
 }
@@ -107,24 +114,29 @@ void CircularDoublyLinkedList::append(int val) {
 	node->next = rear->next;
 	rear->next= node;
 	rear = node;
+	head->prev = rear;
 }
 
 void CircularDoublyLinkedList::del(int item) {
-
 	if (!head)
 		return;
 
 	if (head->val == item)
 		delete_front();
-	else {	
+	else if (rear->val == item)
+		delete_back();
+	else {
 		Node *node_before = head;
 
-		while (node_before != rear && node_before->next->val != item)
+		while (node_before->next->next != rear && node_before->next->val != item)
 			node_before=node_before->next;
 
 		if (node_before->next->val == item) {
 			Node *temp = node_before->next;
-			node_before->next = node_before->next->next;
+			
+			node_before->next->next->prev = node_before;
+			node_before->next = temp->next;
+
 			delete temp;
 		}
 	}
@@ -135,17 +147,23 @@ void CircularDoublyLinkedList::delete_front() {
 		return;
 
 	Node *temp = head;
+
+	rear->next = head->next;
+	head->next->prev = rear;
 	head = head->next;
+
 	delete temp;
 }
 
 void CircularDoublyLinkedList::delete_back() {
-
 	if (!head)
 		return;
+
 	Node *temp = rear;
+
 	rear = rear->prev;
 	rear->next = head;
 	head->prev = rear;
+
 	delete temp;
 }
