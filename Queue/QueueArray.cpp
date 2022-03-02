c30b31b2040b3bd3d65fc353ba55b3ed430464e0
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-03-02 21:53:00 +0200

    Update QueueArray.cpp

--- Queue/QueueArray.cpp
+++ Queue/QueueArray.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 
 class Queue {
-    
-    int *queue;
-public:
     size_t capacity;
     int front, tail;
 
+    int *queue;
+
+public:
     Queue (size_t c) : capacity(c) {
-        tail = front = -1;
+        front = tail = -1;
         queue = new int[c];
     }
 
     bool isFull() {
-        return (front == 0 && tail==capacity-1 || front == tail+1);
+        return (tail+1)%capacity == front;
     }
 
     bool isEmpty() {
-        return (front==-1);
+        return (front == -1);
     }
 
     void enQueue(int val) {
         if (isFull()) {
             std::cout << "Queue is full\n";
         } else {
-            if (front == -1)
+            if (isEmpty())
                 front = 0;
-            tail = (tail + 1) % capacity;
+            tail = (tail + 1)%capacity;
             queue[tail] = val;
         }
     }
@@ -34,25 +34,24 @@ public:
     void deQueue() {
         if (isEmpty()) {
             std::cout << "Queue is empty\n";
-            return;
-        }
-        queue[front] = 0;
-
-        if (front == tail)
-            front = tail = -1;
-        else {
-            front = (front + 1)%capacity;
+        } else {
+            queue[front] = 0;
+            if (front == tail)
+                front = tail = -1;
+            else
+                front = (front+1)%capacity;
         }
     }
 
     void print() {
+        if (isEmpty())
+            return;
         size_t i;
         for (i = front; i != tail; i = (i+1)%capacity)
             std::cout << queue[i] << " ";
-        std::cout << queue[i] << "\n";
+        std::cout << queue[tail] << "\n";
     }
 };
-
 
 int main() {
     Queue q(5);
@@ -60,19 +59,24 @@ int main() {
     q.enQueue(10);
     q.enQueue(20);
     q.enQueue(30);
-    q.print();
-
-    std::cout << "front: " << q.front << " tail: " << q.tail << "\n";
+    q.print(); // 10 20 30
 
     q.deQueue();
     q.deQueue();
-    q.print();
-    std::cout << "front: " << q.front << " tail: " << q.tail << "\n";
+    q.print(); // 30
 
-    q.enQueue(10);
-    q.enQueue(20);
-    q.print();
-    std::cout << "front: " << q.front << " tail: " << q.tail << "\n";
-    
-    return 0;
+    q.enQueue(40);
+    q.enQueue(50);
+    q.enQueue(60);
+    q.enQueue(70);
+    q.enQueue(80);
+    q.print(); // 30 40 50 60 70
+
+    q.deQueue();
+    q.print(); // 40 50 60 70
+
+    q.deQueue();
+    q.deQueue();
+    q.deQueue();
+    q.print(); // 70
 }
