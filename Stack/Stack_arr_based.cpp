1b1b52f0a2eab3b593270809dfbd8ae0d1812063
Badr Kamel <36001332+badrkamel@users.noreply.github.com>
2022-03-21 12:52:50 +0200

    Update Stack_arr_based.cpp

--- Stack/Stack_arr_based.cpp
+++ Stack/Stack_arr_based.cpp
@@ -1,111 +1,44 @@
 #include <iostream>
 
-using namespace std;
-
-class Stack {
-    private:
-    size_t capacity;
+template<typename T> class Stack {
+    size_t CAPACITY;
     size_t top;
-    int *items;
 
-    public:
-    Stack (size_t c) : capacity(c) {
+    T* data;
+
+public:
+    Stack (size_t c) : CAPACITY(c) {
         top = 0;
-        items = new int[c];
+        data = new T[c];
     }
 
-    void push(int value) {
-
-        if (isFull()) {
-            cout << "Error - The stack is full\n";
-            return;
-        }
-        items[top] = value;
-        top++;
+    void push(const T& val) {
+        if (top < CAPACITY)
+        data[top++] = val;
     }
 
     void pop() {
-        items[--top] = 0;
+        if (top)
+        data[--top] = 0;
     }
 
-    bool isFull() {
-        return (top==capacity);
+    void print() {
+        if (top == 0)
+            return;
+        for (size_t i{}; i < top; i++)
+            std::cout << data[i] << "\t";
+        std::cout << "\n";
     }
-
-    bool isEmpty() {
-        return (top==0);
-    }
-
-    int peek() {
-        return items[top-1];
-    }
-
-    int count() {
-        return top;
-    }
-
-    void display() {
-        for (int i=0; i<top; i++)
-            cout << items[i] << "\t";
-        cout << endl;
-    }
-
-    bool search(int item) {
-
-        for (int i=0;i<top; i++) {
-            if (items[i] == item) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 };
 
-int main () {
+int main() {
+    Stack<int> stk(10);
 
-    cout << "Stack ADT\n";
-
-    // Create stack
-    Stack stk(10);
-
-
-    // Check if stack is empty
-    stk.isEmpty() ? cout << "The stack is empty\n" : cout << "The stack has " << stk.count() << " elements\n";
-
-    // Push data
     stk.push(10);
     stk.push(20);
     stk.push(30);
+    stk.print();
 
-    stk.isEmpty() ? cout << "The stack is empty\n" : cout << "The stack has " << stk.count() << " elements\n";
-
-    // Display stack data
-    stk.display();
-
-    // Return last element in the stack
-    cout << stk.peek() << endl;
-
-    // Search
-    stk.search(40) ? cout << "found\n" : cout << "not found!\n";
-
-    // Pop
     stk.pop();
-    stk.isEmpty() ? cout << "The stack is empty\n" : cout << "The stack has " << stk.count() << " elements\n";
-    stk.display();
-
-    /*
-        Methods:
-            - Create
-            - Push
-            - Pop
-            - isEmpty
-            - isFull
-            - Count
-            - Display
-            - peek
-            - Search
-
-    */
-
+    stk.print();
 }
